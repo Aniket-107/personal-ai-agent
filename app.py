07e2b3b1654f00bb682a8a3186480f9139a85c44
Aniket-107 <aniketdash391@gmail.com>
2026-04-20 00:50:35 +0530

    changing the port for deployment

--- app.py
+++ app.py
@@ -50,4 +50,6 @@ def update_task(task_id):
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
